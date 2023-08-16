d7aeaf51cee0e7c0b7895775c630cf8412574aef
Genesis Quinn <genesis@quinns.io>
2023-08-16 16:19:06 -0400

    changed redirect url on oauth.js

--- src/routes/oauth.js
+++ src/routes/oauth.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res, next) => {
 
     const code = req.query.code;
     try {
-        const redirectUrl = 'https://dinnermadeeasy.netlify.app/recipes';
+        const redirectUrl = 'https://dinner-made-easy.onrender.com';
         const oAuth2Client = new OAuth2Client(
             process.env.CLIENT_ID,
             process.env.CLIENT_SECRET,
